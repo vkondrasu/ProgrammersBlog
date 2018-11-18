4fa142bf696745e60c374bcfa44956dd4a8d0224
venkat kondrasu <venkat.kondrasu@gmail.com>
2018-11-18 16:36:52 +0530

    Create binarysearch.js

---
+++ binarysearch.js
@@ -0,0 +1,15 @@
+function binarySearch(Arr, value_to_find, min, max){
+    if( parseInt(min) <= parseInt(max) ){
+        var index = parseInt( (parseInt(min) + parseInt(max) ) / 2 );
+
+        if( parseInt(Arr[index]) === parseInt(value_to_find) ){
+            return index;
+        }
+        else if( parseInt(Arr[index]) > parseInt(value_to_find) ){
+            binarySearch(Arr, value_to_find, min, index - 1);
+        }
+        else{
+            binarySearch(Arr, value_to_find, index+1, max );
+        }
+    }
+}
